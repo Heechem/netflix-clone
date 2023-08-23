b83819db1c32e4072d8f2e03d79fd0cffa145fc5
Heechem <hichempeter@gmail.com>
2023-08-23 08:14:06 -0600

    Add movie Row

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import './Banner.css';
+import axios from './axios';
+import requests from './Requests';
 
 function Banner() {
+  const [movie, setMovie] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(requests.fetchNetflixOriginals);
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
+      return request;
+    }
+    fetchData();
+  }, []);
+
+  console.log(movie);
+
   function truncate(stri, n) {
     return stri?.length > n ? stri.substr(0, n - 1) + '...' : stri;
   }
@@ -9,9 +28,9 @@ function Banner() {
     <header
       className="banner"
       style={{
-        // width: '100vw',
         backgroundSize: 'cover',
-        backgroundImage: `url("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png")`,
+
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
         backgroundPosition: 'center center',
       }}
     >
@@ -22,7 +41,7 @@ function Banner() {
           <button className="banner__button">My list</button>
         </div>
         <h1 className="banner__description">
-          {truncate(`This is a test description`, 150)}
+          {truncate(movie?.overview, 150)}
         </h1>
       </div>
       <div className="banner--faderBottom" />
--- src/HomeScreen.jsx
+++ src/HomeScreen.jsx
@@ -2,12 +2,43 @@ import React from 'react';
 import './HomeScreen.css';
 import Nav from './Nav';
 import Banner from './Banner';
+import requests from './Requests';
+import Row from './Row';
 
 function HomeScreen() {
   return (
     <div className="homescreen">
       <Nav />
       <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row
+        title="Trending Now"
+        fetchUrl={requests.fetchTrending}
+      />
+      <Row
+        title="Action Movies"
+        fetchUrl={requests.fetchActionMovies}
+      />
+      <Row
+        title={'Comedy Movies'}
+        fetchUrl={requests.fetchComedyMovies}
+      />
+      <Row
+        title="Horror Movie"
+        fetchUrl={requests.fetchHorrorMovies}
+      />
+      <Row
+        title="Romance Movie"
+        fetchUrl={requests.fetchRomanceMovies}
+      />
+      <Row
+        title="Documentaries"
+        fetchUrl={requests.fetchDocumentaries}
+      />
     </div>
   );
 }
